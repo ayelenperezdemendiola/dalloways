d3215bd0c5c2c27a503b9b57156f974783a720f1
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2019-11-14 16:10:06 +0100

    docs folder

---
+++ docs/precache-manifest.69c2b068a9a2cd76c4f10d35253b4955.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed6ec424d24e39f2ec48b5b4f591fd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1080354f143260f97f51",
+    "url": "/static/css/main.8e47c79f.chunk.css"
+  },
+  {
+    "revision": "300541a07b55704a9419",
+    "url": "/static/js/2.43f87724.chunk.js"
+  },
+  {
+    "revision": "1080354f143260f97f51",
+    "url": "/static/js/main.5c81b3f1.chunk.js"
+  },
+  {
+    "revision": "4597497c15768494f2e9",
+    "url": "/static/js/runtime-main.bc8df456.js"
+  },
+  {
+    "revision": "62e93d1db577821fedd705b303b396d3",
+    "url": "/static/media/background.62e93d1d.jpg"
+  },
+  {
+    "revision": "b1f2a76280a27ad7ff98924882c41e78",
+    "url": "/static/media/close--btn.b1f2a762.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu--btn.b3f49afd.svg"
+  },
+  {
+    "revision": "3b30c82204fe750dcaa3c5113585f6ee",
+    "url": "/static/media/up--btn.3b30c822.svg"
+  }
+]);
